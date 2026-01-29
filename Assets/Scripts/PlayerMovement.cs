1160e94d22ce3a3b7605c517e4f778174824e0cd
Moein <moeinkhanmohamadi3@gmail.com>
2026-01-29 22:43:56 +0330

    Set limits for player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,16 @@ public class PlayerMovement : MonoBehaviour
     {
         // Move left or right
         transform.position += Input.GetAxis("Horizontal") * Vector3.right * movementSpeed * Time.deltaTime;
+
+        // Set limits for movement
+        if (transform.position.x > 2.5f)
+        {
+            transform.position = new Vector3(2.5f, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x < -2.5f)
+        {
+            transform.position = new Vector3(-2.5f, transform.position.y, transform.position.z);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
